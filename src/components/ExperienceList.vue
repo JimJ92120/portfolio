c88dadf11b6ea47ee26365034efdcc92ff0eab4a
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-06 16:49:40 +0700

    refactor(experience list): remove text margin

--- src/components/ExperienceList.vue
+++ src/components/ExperienceList.vue
@@ -6,7 +6,7 @@
           {{ experience.company }} | {{ experience.position }}
         </v-card-title>
         <v-card-subtitle>{{ experience.period }}</v-card-subtitle>
-        <v-card-text style="white-space: pre-wrap" class="my-3">
+        <v-card-text style="white-space: pre-wrap" class="mt-3">
           {{ experience.description }}
         </v-card-text>
       </v-card>
