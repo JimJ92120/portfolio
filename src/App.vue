f7b6e083a0cc11ef13749204c87bd15cbcd406ba
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-06 11:18:39 +0700

    chore(vue): remove store and router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <h1>Sidebar</h1>
+        </v-col>
+        <v-col>
+          <v-main>
+            <h1>Hello World</h1>
+          </v-main>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
