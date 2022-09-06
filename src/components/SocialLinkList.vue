c05a80f2615024801a32b1919426e9047ea5835a
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-06 14:11:21 +0700

    refactor(social link): update icon size

--- src/components/SocialLinkList.vue
+++ src/components/SocialLinkList.vue
@@ -3,7 +3,7 @@
     <v-list-item v-for="(link, key) in links" :key="key">
       <v-container>
         <v-btn icon :href="link.url" target="_blank">
-          <v-icon>{{ link.icon }}</v-icon>
+          <v-icon size="large">{{ link.icon }}</v-icon>
         </v-btn>
       </v-container>
     </v-list-item>
