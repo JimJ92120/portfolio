cc0cbd59eef6b3e386f745d1e8d592df5c2f8855
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-19 08:29:05 +0700

    feat(project card): add component to render projects and demos

---
+++ src/components/demos/ProjectCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card>
+    <v-card-title class="">{{ project.title }}</v-card-title>
+    <v-card-subtitle>{{ project.description }}</v-card-subtitle>
+    <DevIconList :icon-slug-list="project.technologies" />
+
+    <v-divider></v-divider>
+
+    <v-card-actions class="d-flex">
+      <v-breadcrumbs :items="breadcrumbs" divider="|" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DevIconList from "@/components/base/DevIconList.vue";
+
+export default defineComponent({
+  name: "ProjectCard",
+  components: {
+    DevIconList,
+  },
+  props: ["project"],
+  computed: {
+    breadcrumbs: function () {
+      return [
+        {
+          title: "View demo",
+          href: this.project.demoUrl,
+          target: "_blank",
+          disabled: false,
+        },
+        {
+          title: "See repository",
+          href: this.project.repositoryUrl,
+          target: "_blank",
+          disabled: false,
+        },
+      ];
+    },
+  },
+});
+</script>
+
+<style>
+.v-card-subtitle {
+  text-overflow: unset;
+  white-space: pre-wrap;
+}
+</style>
