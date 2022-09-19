26b21ddc8f105102b0003a943963d70e61efc55d
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-19 08:29:39 +0700

    feat(demo): add projects data to page

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,13 +1,30 @@
 <template>
   <v-container id="demo">
-    <h1>Some demos...</h1>
+    <h1>Demos & Projects</h1>
+
+    <v-row class="mt-4">
+      <v-col cols="12" md="4" v-for="(projectData, key) in projects" :key="key">
+        <ProjectCard :project="projectData" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ProjectCard from "@/components/demos/ProjectCard.vue";
+
+import projectsData from "@/data/projects.json";
 
 export default defineComponent({
   name: "DemoView",
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      projects: projectsData,
+    };
+  },
 });
 </script>
