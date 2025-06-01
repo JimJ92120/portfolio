306fe571bdaff146d4e35cf51e1e0f203d39cefc
JimJ92120 <jimj.development@gmail.com>
2025-06-01 14:20:41 +0700

    chore(init): setup project with webpack, ts

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const app: HTMLElement = document.querySelector("#app")!;
+
+  app.innerHTML = `
+    <h1>Portfolio</h1>
+
+    <p>Hello World</p>
+  `;
+});
