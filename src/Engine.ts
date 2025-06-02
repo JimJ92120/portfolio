b99c1a000ec10d9f54ac1508595548094fe777c9
JimJ92120 <jimj.development@gmail.com>
2025-06-02 13:54:56 +0700

    feat: add home frame and required actions

--- src/Engine.ts
+++ src/Engine.ts
@@ -2,15 +2,22 @@ import Frame from "./Frame";
 import Player from "./Player";
 
 export type FrameRecord = { main: Frame; [key: string]: Frame };
+type FrameActionRecord = { [key: string]: CallableFunction };
+export type ActionRecord = {
+  main: FrameActionRecord;
+  [key: string]: FrameActionRecord;
+};
 
 export default class Engine {
   frames: FrameRecord;
-  currentFrame: Frame;
+  actions: ActionRecord;
+  currentFrameKey: string;
   player: Player;
 
-  constructor(frames: FrameRecord, player: Player) {
+  constructor(frames: FrameRecord, player: Player, actions: ActionRecord) {
     this.frames = frames;
     this.player = player;
+    this.actions = actions;
 
     this.setCurrentFrame("main");
   }
@@ -21,21 +28,32 @@ export default class Engine {
       this.player.position[1] + direction[1],
     ];
 
-    if (this.canPlayerMove(nextPosition)) {
-      this.player.position = nextPosition;
-      this.player.direction = direction;
-
-      return true;
+    if (!this.canPlayerMove(nextPosition)) {
+      return false;
     }
 
-    return false;
+    if (
+      this.actions[this.currentFrameKey] &&
+      this.actions[this.currentFrameKey][nextPosition.join(",")]
+    ) {
+      this.actions[this.currentFrameKey][nextPosition.join(",")]();
+    } else {
+      this.player.position = nextPosition;
+      this.player.direction = direction;
+    }
+
+    return true;
   }
 
   setCurrentFrame(frameKey: string): void {
     if (this.frames[frameKey]) {
-      this.currentFrame = this.frames[frameKey];
+      this.currentFrameKey = frameKey;
       this.player.position = this.currentFrame.initialPosition;
     }
+  }
+
+  get currentFrame(): Frame {
+    return this.frames[this.currentFrameKey];
   }
 
   private canPlayerMove(nextPosition: [number, number]): boolean {
@@ -60,7 +78,9 @@ export default class Engine {
       [1, 2].includes(nextPositionCellValue)
     ) {
       console.error(
-        `nextPosition [${nextPosition}] with value ${nextPositionCellValue} is a block or building`
+        `nextPosition [${nextPosition}] with value ${nextPositionCellValue} is a ${
+          1 === nextPositionCellValue ? "block" : "building"
+        }`
       );
 
       return false;
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from "./App";
 import Renderer from "./Renderer";
 import Frame from "./Frame";
 import Player from "./Player";
-import Engine, { FrameRecord } from "./Engine";
+import Engine, { ActionRecord, FrameRecord } from "./Engine";
 
 const TILE_SIZE: [number, number] = [25, 25];
 
@@ -34,8 +34,57 @@ document.addEventListener("DOMContentLoaded", () => {
       ],
       [5, 8]
     ),
+    home: new Frame(
+      [
+        [1, 1, 1, 1, 1, 1],
+        [1, 2, 0, 3, 3, 1],
+        [1, 3, 0, 0, 0, 1],
+        [1, 2, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 1, 3, 3, 1, 1],
+      ],
+      [2, 4]
+    ),
   };
-  const engine = new Engine(frameRecord, new Player());
+  const actionRecord: ActionRecord = {
+    main: {
+      ["2,2"]: () => {
+        alert("office coming soon");
+      },
+      ["8,3"]: () => {
+        alert("gallery coming soon");
+      },
+      ["8,4"]: () => {
+        alert("gallery coming soon");
+      },
+      ["2,7"]: () => {
+        console.log("enter home");
+
+        engine.setCurrentFrame("home");
+        renderer.resize(engine.currentFrame.width, engine.currentFrame.height);
+      },
+    },
+    home: {
+      ["1,2"]: () => {
+        alert("here's a desk");
+      },
+      ["3,1"]: () => {
+        alert("here's an object");
+      },
+      ["4,1"]: () => {
+        alert("here's another object");
+      },
+      ["2,5"]: () => {
+        engine.setCurrentFrame("main");
+        renderer.resize(engine.currentFrame.width, engine.currentFrame.height);
+      },
+      ["3,5"]: () => {
+        engine.setCurrentFrame("main");
+        renderer.resize(engine.currentFrame.width, engine.currentFrame.height);
+      },
+    },
+  };
+  const engine = new Engine(frameRecord, new Player(), actionRecord);
 
   renderer.resize(engine.currentFrame.width, engine.currentFrame.height);
   renderer.renderEngine(engine);
