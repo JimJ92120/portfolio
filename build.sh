4f5d23e47a564de094aa772e56f918d5cdfc18a9
JimJ92120 <jimj.development@gmail.com>
2025-06-02 23:03:00 +0700

    chore: add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+rm -rf index.*
+
+mkdir build
+git clone https://github.com/JimJ92120/portfolio.git build
+
+npm install --prefix=./build
+npm run build --prefix=./build
+
+cp -r ./build/dist/* .
+
+rm -rf ./build
